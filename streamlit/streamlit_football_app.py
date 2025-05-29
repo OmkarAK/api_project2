21ad8aa06cd484c0492cbaa0140946c003d187cc
OmkarAK <77271316+OmkarAK@users.noreply.github.com>
2025-05-29 14:25:35 +0000

    -message "changes made by adding render as API host"

--- streamlit/streamlit_football_app.py
+++ streamlit/streamlit_football_app.py
@@ -3,7 +3,7 @@ import logging
 import pandas as pd
 
 if 'base_url' not in st.session_state:
-    st.session_state['base_url'] = "https://literate-acorn-5947rjg9r7q274vw-8000.app.github.dev" 
+    st.session_state['base_url'] = "https://api-project2-q4xl.onrender.com" 
 
 logging.basicConfig(
     filename='football_app.log',  
